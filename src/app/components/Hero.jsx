d0c6c9431047e6ea7b557f68af7db3708bbc0c09
Halanizi <halanizi@gmail.com>
2024-12-02 17:06:53 -0800

    Initial commit

---
+++ src/app/components/Hero.jsx
@@ -0,0 +1,3 @@
+export default function Hero() {
+    return <div>hello world this is my app</div>;
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,24 @@
 import type { Config } from "tailwindcss";
+import colors from "tailwindcss/colors";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+    theme: {
+        colors: {
+            white: colors.white,
+            gray: colors.gray,
+            periwinkle: "#D8DCFF",
+            darkperiwinkle: "#AEADF0",
+            rosybrown: "#C38D94",
+            chinarose: "#A76571",
+            ultraviolet: "#565676",
+        },
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 } satisfies Config;
